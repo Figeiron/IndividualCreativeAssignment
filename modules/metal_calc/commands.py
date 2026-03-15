37f988d136478777c91981d3a6431c3b4a10ca46
Figeiron <stroookr6@gmail.com>
2026-03-15 22:39:52 +0200

    added base metal calc class

--- modules/metal_calc/commands.py
+++ modules/metal_calc/commands.py
@@ -1,14 +1,20 @@
 from core.command import Command
 from core.parameter import Parameter, RangeValidator, IndexParameter
+from abc import ABC, abstractmethod
 
+class CalculateMetalCommand(Command):
+    @classmethod
+    def get_params(cls, service=None):
+
+        materials_count = len(service.materials)
+        return [ 
+            IndexParameter("material_index", "Індекс матеріалу", f"від 1 до {materials_count}", int,
+                           validators=[RangeValidator(1, materials_count)]),
+        ]
 
 class GetMaterialsCommand(Command):
     name = "Довідник матеріалів"
     description = "Показати доступні тонколистові метали"
-
-    @classmethod
-    def get_params(cls, service=None):
-        return []
 
     def __init__(self, service):
         self.service = service
@@ -17,20 +23,16 @@ class GetMaterialsCommand(Command):
         return self.service.get_materials()
 
 
-class CalculatePipeCommand(Command):
+class CalculatePipeCommand(CalculateMetalCommand):
     name = "Розрахувати трубу"
     description = "Розрахувати розгортку та вартість труби"
 
     @classmethod
     def get_params(cls, service=None):
-        materials_count = len(service.materials)
-        return [
+        return super().get_params(service) + [
             Parameter("diameter", "Діаметер", "Діаметер труби в мм", float, validators=[RangeValidator(min_val=1)]),
-            Parameter("length", "Довжина", "Довжина труби в мм", float, validators=[RangeValidator(min_val=1)]),
-            IndexParameter("material_index", "Індекс матеріалу", f"від 1 до {materials_count}", int,
-                           validators=[RangeValidator(1, materials_count)]),
+            Parameter("length", "Довжина", "Довжина труби в мм", float, validators=[RangeValidator(min_val=1)])
         ]
-
     def __init__(self, service, diameter, length, material_index):
         self.service = service
         self.diameter = diameter
