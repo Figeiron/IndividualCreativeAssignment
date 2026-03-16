4860087342aad882b91d2df43750cae07690fca5
Figeiron <stroookr6@gmail.com>
2026-03-16 10:38:04 +0200

    Organize about commands

--- modules/about/commands.py
+++ modules/about/commands.py
@@ -1,31 +1,28 @@
 from core.command import Command
+from abc import ABC
 
 
-class GetAboutCreator(Command):
+class GetAboutCommand(Command, ABC):
+    def __init__(self, service):
+        self.service = service
+
+class GetAboutCreatorCommand(GetAboutCommand):
     name = "Про розробника"
     description = "Про розробника"
 
-    @classmethod
-    def get_params(cls, service):
-        return []
-
     def __init__(self, service):
-        self.service = service
+        super().__init__(service)
 
     def _execute(self, context):
         return self.service.get_about_creator()
 
 
-class GetAboutProgram(Command):
+class GetAboutProgramCommand(GetAboutCommand):
     name = "Про програму"
     description = "Про програму"
 
-    @classmethod
-    def get_params(cls, service):
-        return []
-
     def __init__(self, service):
-        self.service = service
+        super().__init__(service)
 
     def _execute(self, context):
         return self.service.get_about_program()
